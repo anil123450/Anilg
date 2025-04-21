4db715612eb17e7451c847407eb558c35aad25ea
anil123450 <gadgeanil007@gmail.com>
2025-04-21 12:17:53 +0530

    Update home page

--- tests/home-page.spec.js
+++ tests/home-page.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test')
 
-test('My first playwright',async ({browser}) => {
+test.only('My first playwright',async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     const userName = page.locator('#username');
